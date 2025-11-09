76d4cdb09f9512e01e89d421995a18b6168d76c0
1abcd-efgh <ae9913609@gmail.com>
2025-11-09 17:45:27 +0530

    Update 3 bank account.sol

--- BT/3 bank account.sol
+++ BT/3 bank account.sol
@@ -44,3 +44,32 @@ contract BankAccount {
 
 }
 
+
+//2nd type easy
+pragma solidity ^0.8.0; 
+contract Bank { 
+    mapping(address => uint256) private balances; 
+
+    // Deposit money 
+    function deposit() public payable {
+        require(msg.value > 0, "Deposit must be greater than 0");
+        balances[msg.sender] += msg.value;
+    }
+
+    // Withdraw money 
+    function withdraw(uint256 amount) public { 
+        require(amount <= balances[msg.sender], "Insufficient balance"); 
+        balances[msg.sender] -= amount; 
+        payable(msg.sender).transfer(amount); 
+    } 
+
+    // Show customer balance 
+    function getBalance() public view returns (uint256) { 
+        return balances[msg.sender]; 
+    } 
+
+    // Show contract balance 
+    function getContractBalance() public view returns (uint256) { 
+        return address(this).balance; 
+    } 
+}
